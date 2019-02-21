55e842a6ea2bbbe39bf8316f7ecb362c67a5c5fb
lilvina <davinaleo2007@yahoo.com>
2019-02-20 22:12:47 -0800

    working on some of the styling

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,9 +3,20 @@ import './SearchBar.css';
 
 const SearchBar = () => {
   return (
-    <form className="search">
-      <input type="text" placeholder="search.."/>
-    </form>
+    <div className="search-bar">
+      <div className="instagram-logo">
+        <img src="https://i.imgur.com/RzOn2Qz.png" alt="logo"/>
+      </div>
+      <form className="search">
+        <input type="text" placeholder="search.."/>
+      </form>
+      <div className="icons">
+        <i className="far fa-compass" />
+        <i className="far fa-heart" />
+        <i className="far fa-user" />
+      </div>
+    </div>
+
   )
 }
 
